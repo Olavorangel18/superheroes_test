7720b6dd06b8d1af3b3fb4e93ede95ef06a3c65f
Olavo Conceicao <orc@cin.ufpe.br>
2023-10-08 20:51:06 -0300

    <fix> bug: fixed bug in main class test

--- src/test/java/com/w2m/superheroes/superheroes_test/SuperheroesTestApplicationTests.java
+++ src/test/java/com/w2m/superheroes/superheroes_test/SuperheroesTestApplicationTests.java
@@ -2,10 +2,13 @@ package com.w2m.superheroes.superheroes_test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest
+import com.superheroes.superheroes_test.SuperheroesTestApplication;
+
+@SpringBootTest(classes = SuperheroesTestApplication.class)
+@ComponentScan(basePackages = {"com.w2m.superheroes.*"})
 class SuperheroesTestApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
