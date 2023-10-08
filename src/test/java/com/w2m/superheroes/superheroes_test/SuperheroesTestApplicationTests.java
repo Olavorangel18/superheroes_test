464c9417d30b1467b944ce6d706e1e75a38b951a
Olavo Conceicao <orc@cin.ufpe.br>
2023-10-08 08:40:08 -0300

    <app> creacion de la aplicacion

---
+++ src/test/java/com/w2m/superheroes/superheroes_test/SuperheroesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.superheroes.superheroes_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
